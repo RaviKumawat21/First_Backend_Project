48b4329093884934b9b826241f27c44d67f6d6a4
RaviKumawat21 <ravikumawat8860@gmail.com>
2025-08-02 21:45:29 +0530

    access and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,6 +65,35 @@ userSchema.pre("save", async function(next){
     next();
 })
 
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
 
+userSchema.methods.generateAccessToken  = function(){
+    jwt.sign({
+        _id : this._id,
+        username : this.username,
+        email : this.email,
+        fullName : this.fullName,
+        
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    })};
 
+    userSchema.methods.generateRefreshToken  = function(){
+        jwt.sign({
+            _id : this._id,
+            username : this.username,
+            email : this.email,
+            fullName : this.fullName,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+        });
+
+}
 export const User = mongoose.model("User", userSchema);
